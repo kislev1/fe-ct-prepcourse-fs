5dd1d0e0685dfc8e8a1f6f5e3e971363cdec5743
Kislev <kislevgonzaga@gmail.com>
2023-07-27 11:58:21 -0600

    avance homework

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,7 +1,8 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
+function devolverString(string) { 
+   return (string)
    // Debe retornar un string.
    // Tu código:
 }
@@ -10,27 +11,32 @@ function devolverString(string) {
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // Tu código: 
+   return (x + y)
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return (x - y)
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return (x / y)
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return (x * y)
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return (x % y)
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
